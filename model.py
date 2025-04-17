23c2606af5a846df510dde15a32a2e8cdf3a171b
P-K-PRAJHAN <pkprajhan@gmail.com>
2025-04-17 10:31:36 +0530

    commit\

--- model.py
+++ model.py
@@ -1,15 +1,22 @@
 import pandas as pd
 from sklearn.datasets import load_iris
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+import matplotlib.pyplot as plt
 import joblib
 
 def train_model():
     iris = load_iris()
     X = iris.data
     y = iris.target
-    model = DecisionTreeClassifier()
-    model.fit(X, y)
-    joblib.dump(model, "decision_tree_model.pkl")
+    clf = DecisionTreeClassifier()
+    clf.fit(X, y)
+    joblib.dump(clf, "decision_tree_model.pkl")
+
+    # Plot and save the decision tree
+    plt.figure(figsize=(12, 8))
+    plot_tree(clf, filled=True, feature_names=iris.feature_names, class_names=iris.target_names)
+    plt.savefig("static/tree.png")
+    plt.close()
 
 if __name__ == "__main__":
     train_model()
